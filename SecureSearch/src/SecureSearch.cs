37056f19d4467b521fcbde7f40d07a66f8e4176c
cgood404 <chrisgood404@gmail.com>
2019-01-11 13:52:39 -0500

    brainstorming

--- SecureSearch/src/SecureSearch.cs
+++ SecureSearch/src/SecureSearch.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+// https://weblog.west-wind.com/posts/2009/Nov/13/Capturing-and-Transforming-ASPNET-Output-with-ResponseFilter
+// https://weblog.west-wind.com/posts/2004/Jun/08/Capturing-Output-from-ASPNet-Pages
+
+// Alternatively, intercept incoming requests to the API and wrap the request, write an application that makes a new request and filters the response, then sends that back to the user
+
 namespace SecureSearch
 {
-    public class SecureSearch
+    public class SecureSearch : IHttpHandler
     {
+        public bool IsReusable { get { return true; } }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
